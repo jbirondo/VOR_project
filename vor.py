0efe844248733a3a7310071c5d847160841e7907
Eugene Birondo <jbirondo@gmail.com>
2020-08-24 21:41:32 -0700

    Create dataframe ran into error

--- vor.py
+++ vor.py
@@ -1,1 +1,8 @@
+# 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/fp_projections.csv')
+
+print(df.head(10))
