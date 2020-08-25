5b76425e59e38df81e846cbd55cb8ddfb5efe7fa
Eugene Birondo <jbirondo@gmail.com>
2020-08-24 22:54:18 -0700

    Using .loc

--- vor.py
+++ vor.py
@@ -34,4 +34,23 @@ df['FantasyPoints'] = (
     df['Int']*scoring_weights['int'] 
     )
 
-print(df[:5])
+
+# # mask our dataframe based off a position
+# """
+# .loc is a way of getting back specified cross sections of your dataframe.
+# The syntax is as follows:
+# new_df = old_df.loc[row_indexer, column_indexer]
+# Where row_indexer can take the form of a boolean indexer.
+# For example, df['Pos'] == 'RB'
+# or, df['RushingAtt'] > 20
+# or, df['Pos'].isin(['QB', 'WR', 'RB', TE]) # check if a player's position is a skill position
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html # docs on loc
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html # docs on indexing
+# """
+
+rb_df = df.loc[df['Pos'] == 'RB']
+# rb_df = rb_df['RushingAtt'] > 250
+
+print(rb_df.head())
+
+# print(df[:5])
