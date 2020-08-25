5203eb1a7ca7a1030b77cdf62bd630e07179ea19
Eugene Birondo <jbirondo@gmail.com>
2020-08-24 21:59:17 -0700

    Testing columns

--- vor.py
+++ vor.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/fp_projections.csv')
 
+#.iloc[] is primarily integer position based (from 0 to length-1 of the axis), but may also be used with a boolean array.
+#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iloc.html
 df = df.iloc[:, 1:]
 
-print(df[:5])
+print(' '.join((df.columns)))
