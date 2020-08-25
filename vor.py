ddfe851212277477fe1477377be2a11fce9aea3b
Eugene Birondo <jbirondo@gmail.com>
2020-08-24 21:47:35 -0700

    Added hotfix for disabling ssl checking

--- vor.py
+++ vor.py
@@ -1,6 +1,7 @@
-# 
+#hotfix for disabling ssl checking 
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/fantasydatapros/data/master/fantasypros/fp_projections.csv')
